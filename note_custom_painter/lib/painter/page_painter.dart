a59a9a2d1e924e63c0558a85b68d003dfee99a4c
Fardeen Khan <khanfardeen9983@gmail.com>
2019-07-05 13:52:41 +0530

    Draw the page with 4 lines

--- note_custom_painter/lib/painter/page_painter.dart
+++ note_custom_painter/lib/painter/page_painter.dart
@@ -3,10 +3,30 @@ import 'package:flutter/material.dart';
 class PagePainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
+    //Draw a grey rounded rectangle on full sized canvas
     var paint = Paint()..color = Colors.grey;
     var rect =
         RRect.fromLTRBR(0, 0, size.width, size.height, Radius.circular(8.0));
     canvas.drawRRect(rect, paint);
+
+    //Create another white rounded rectangle over the previous one with margin from left
+    paint = Paint()
+      ..color = Colors.white;
+    rect = RRect.fromLTRBR(6, 0, size.width, size.height, Radius.circular(8.0));
+    canvas.drawRRect(rect, paint);
+
+    //Draw horizontal lines on the white page
+    paint = Paint()
+      ..color = Colors.blueGrey
+      ..strokeWidth = 1.0;
+    canvas.drawLine(Offset(6, size.height * .2),
+        Offset(size.width, size.height * .2), paint);
+    canvas.drawLine(Offset(6, size.height * .4),
+        Offset(size.width, size.height * .4), paint);
+    canvas.drawLine(Offset(6, size.height * .6),
+        Offset(size.width, size.height * .6), paint);
+    canvas.drawLine(Offset(6, size.height * .8),
+        Offset(size.width, size.height * .8), paint);
   }
 
   @override
