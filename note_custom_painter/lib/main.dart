87dd246bda9f068990da594fe5ecb4eb3d68193b
Fardeen Khan <khanfardeen9983@gmail.com>
2019-07-05 13:25:28 +0530

    Starting with a new Custom Painter app

---
+++ note_custom_painter/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'pages/home_page.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: "Notes", routes: {"/": (context) => HomePage()});
+  }
+}
